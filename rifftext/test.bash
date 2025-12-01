41894542aef00c870bd063bd1dd14b60cba4acf9
Ben L. Titzer <ben.titzer@gmail.com>
2025-12-01 01:43:52 -0500

    Add some operators

--- rifftext/test.bash
+++ rifftext/test.bash
@@ -3,4 +3,4 @@
 VIRGIL_LOC="$HOME/virgil/"
 LIBS="$VIRGIL_LOC/lib/test/*.v3 $VIRGIL_LOC/lib/util/*.v3"
 
-v3i $V3C_OPTS -fun-exprs RiffIr.v3 RiffParser.v3 Unittest.v3 $LIBS
+v3i $V3C_OPTS -fun-exprs RiffIr.v3 RiffParser.v3 Unittest.v3 Operators.v3 $LIBS
