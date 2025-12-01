28f528e63e5d335d7cf01fbab3f1f98ec66bca0d
Ben L. Titzer <ben.titzer@gmail.com>
2025-12-01 13:54:34 -0500

    Fix negative steps

--- rifftext/test.bash
+++ rifftext/test.bash
@@ -3,4 +3,9 @@
 VIRGIL_LOC="$HOME/virgil/"
 LIBS="$VIRGIL_LOC/lib/test/*.v3 $VIRGIL_LOC/lib/util/*.v3"
 
+if [ "$1" = "-fatal" ]; then
+    V3C_OPTS="$V3C_OPTS -redef-field=UnitTests.fatal=true"
+    shift
+fi
+
 v3i $V3C_OPTS -fun-exprs RiffIr.v3 RiffParser.v3 Unittest.v3 Operators.v3 $LIBS
